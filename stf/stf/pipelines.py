4b5980982c3fe66d6842b793cf8f6b2cc07563e9
Bruno Justino <brunojustin@gmail.com>
2018-08-12 00:22:52 -0300

    Save data and Verify if exists in database

--- stf/stf/pipelines.py
+++ stf/stf/pipelines.py
@@ -5,6 +5,13 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import sys
+import MySQLdb
+import MySQLdb.cursors
+import hashlib
+from scrapy.exceptions import DropItem
+from scrapy.http import Request
+import logging
 
 class StfPipeline(object):
     def process_item(self, item, spider):
@@ -13,3 +20,31 @@ class StfPipeline(object):
 class JurisPipeline(object):
     def process_item(self, item, spider):
         return item
+
+class MySQLStorePipeline(object):
+    
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler.stats)
+
+    def __init__(self, stats):
+        #Instantiate DB
+        self.conn = MySQLdb.connect(user='root', passwd='121294', db='stf', host='127.0.0.1', charset="utf8", use_unicode=True)
+        self.cursor = self.conn.cursor()
+        self.logger = logging.getLogger()
+
+
+    def process_item(self, item, spider):       
+        
+        self.cursor.execute("SELECT * FROM processos WHERE data=%s AND observacao=%s" , (item['data'],item['observacao'],))
+
+        processo = self.cursor.fetchone()
+
+        if not processo:         
+            self.cursor.execute("""INSERT INTO processos (data,andamento,orgao_julgador,observacao,documento) VALUES (%s,%s,%s,%s,%s)""", (item['data'],item['andamento'], item['orgao_julgador'],item['observacao'],item['documento']))
+            self.conn.commit()
+        else:
+            print('cu')
+            self.logger.info('Já existe esse andamento na base de dados')       
+
+
--- stf/stf/settings.py
+++ stf/stf/settings.py
@@ -66,6 +66,7 @@ ROBOTSTXT_OBEY = True
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
    'stf.pipelines.JurisPipeline': 300,
+   'stf.pipelines.MySQLStorePipeline':500
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
